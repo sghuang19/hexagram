de74afaeb90110bc228a50bb23ec6a11163ca53f
Samuel Huang <samuel.g.huang@outlook.com>
2024-08-04 11:10:06 -0400

    Dev env config files

---
+++ eslint.config.js
@@ -0,0 +1,22 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
+import pluginReactJsxRuntime from "eslint-plugin-react/configs/jsx-runtime.js";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+
+export default [
+  { files: ["**/*.{js,jsx}"] },
+  { languageOptions: { globals: globals.browser } },
+  { settings: { react: { version: "detect" } } },
+  pluginJs.configs.recommended,
+  pluginReactConfig,
+  pluginReactJsxRuntime,
+  eslintPluginPrettierRecommended,
+  {
+    rules: {
+      "prettier/prettier": "error",
+      "no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+      "react/prop-types": "off",
+    },
+  },
+];
