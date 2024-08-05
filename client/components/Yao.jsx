4a39ae68a724a812cd1eb9decf077aa8dc7b3f5c
Samuel Huang <hi@sghuang.com>
2024-08-05 19:47:45 -0400

    Restructure Yao and Hexagram with interactivity

--- client/components/Yao.jsx
+++ client/components/Yao.jsx
@@ -1,9 +1,35 @@
-import YinYao from "./YinYao.jsx";
-import YangYao from "./YangYao.jsx";
+import useSelectedLine from "../hooks/useSelectedLine.js";
 
-const Yao = ({ type }) => {
-  // `class="yao yin/yang" are included in <YangYao /> and <YinYao />
-  return type ? <YangYao /> : <YinYao />;
-};
+/**
+ * @param {boolean} lineType - true for yang, false for yin
+ * @param {number} lineIndex - 1 to 6, 0 is reserved for no selection state
+ * @return {JSX.Element}
+ * @constructor
+ */
+export default function Yao({ lineType, lineIndex }) {
+  const [selectedLine, setSelectedLine] = useSelectedLine();
 
-export default Yao;
+  const handleClick = (event, lineIndex) => {
+    event.stopPropagation(); // stop event from propagating to somewhere else
+    if (selectedLine === lineIndex) setSelectedLine(0);
+    else setSelectedLine(lineIndex);
+    console.log(`Line clicked: ${lineIndex}, now ${selectedLine} is selected`);
+  };
+
+  return (
+    <div
+      className={`yao ${lineType ? "yang" : "yin"} ${selectedLine === lineIndex ? "selected" : ""}`}
+      onClick={(event) => {
+        handleClick(event, lineIndex);
+      }}
+    >
+      {lineType ? null : (
+        <>
+          <div className="yin-part"></div>
+          <div className="yin-gap"></div>
+          <div className="yin-part"></div>
+        </>
+      )}
+    </div>
+  );
+}
