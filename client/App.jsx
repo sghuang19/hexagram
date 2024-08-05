13ae48eecfc57b71e81941a3ffb3920e554d0639
Samuel Huang <hi@sghuang.com>
2024-08-04 22:01:04 -0400

    Remove dev support from bottom of App.jsx

--- client/App.jsx
+++ client/App.jsx
@@ -30,8 +30,4 @@ function App() {
 
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(
-  <DevSupport ComponentPreviews={ComponentPreviews} useInitialHook={useInitial}>
-    <App />
-  </DevSupport>,
-);
+root.render(<App />);
